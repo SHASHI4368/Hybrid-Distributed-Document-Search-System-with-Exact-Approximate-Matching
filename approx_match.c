1234edb619de17ac3a95296fe9d22e0128e49b04
SHASHI4368 <shashikagurunayake3198@gmail.com>
2025-07-15 07:54:13 +0530

    approximate search updated

--- approx_match.c
+++ approx_match.c
@@ -1,37 +1,71 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <math.h>
 
-int levenshtein(const char *s1, const char *s2)
-{
-  int m = strlen(s1), n = strlen(s2);
-  int dp[m + 1][n + 1];
-  for (int i = 0; i <= m; i++)
-    dp[i][0] = i;
-  for (int j = 0; j <= n; j++)
-    dp[0][j] = j;
+#define MAX_WORD 256
+#define MAX_DIST 2  // allowed Levenshtein distance
 
-  for (int i = 1; i <= m; i++)
-    for (int j = 1; j <= n; j++)
-      dp[i][j] = (s1[i - 1] == s2[j - 1]) ? dp[i - 1][j - 1] : 1 + fmin(dp[i - 1][j], fmin(dp[i][j - 1], dp[i - 1][j - 1]));
-  return dp[m][n];
+// Convert string to lowercase (in-place)
+void to_lower_str(char *s) {
+    for (int i = 0; s[i]; i++)
+        s[i] = tolower((unsigned char)s[i]);
 }
 
-int approx_match(const char *filepath, const char *pattern)
-{
-  FILE *fp = fopen(filepath, "r");
-  if (!fp)
-    return 0;
-  char word[256];
-  while (fscanf(fp, "%255s", word) == 1)
-  {
-    if (levenshtein(word, pattern) <= 2)
-    {
-      fclose(fp);
-      return 1;
+// Duel-and-Sweep style Levenshtein with early abort
+int bounded_levenshtein(const char *s1, const char *s2, int max_dist) {
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+
+    // Early cutoff if lengths differ too much
+    if (abs(len1 - len2) > max_dist) return max_dist + 1;
+
+    int dp[MAX_WORD + 1];
+    for (int j = 0; j <= len2; j++) dp[j] = j;
+
+    for (int i = 1; i <= len1; i++) {
+        int prev = dp[0];
+        dp[0] = i;
+        int min_in_row = dp[0];
+
+        for (int j = 1; j <= len2; j++) {
+            int tmp = dp[j];
+            if (tolower(s1[i - 1]) == tolower(s2[j - 1])) {
+                dp[j] = prev;
+            } else {
+                dp[j] = 1 + fmin(fmin(dp[j], dp[j - 1]), prev);
+            }
+            prev = tmp;
+
+            if (dp[j] < min_in_row) min_in_row = dp[j];
+        }
+
+        // Duel phase: give up early if best score already too high
+        if (min_in_row > max_dist) return max_dist + 1;
     }
-  }
-  fclose(fp);
-  return 0;
+
+    return dp[len2];
+}
+
+int approx_match(const char *filepath, const char *pattern) {
+    FILE *fp = fopen(filepath, "r");
+    if (!fp) return 0;
+
+    char word[MAX_WORD];
+    char lower_pattern[MAX_WORD];
+    strncpy(lower_pattern, pattern, MAX_WORD - 1);
+    lower_pattern[MAX_WORD - 1] = '\0';
+    to_lower_str(lower_pattern);  // pattern to lowercase
+
+    while (fscanf(fp, "%255s", word) == 1) {
+        to_lower_str(word);  // normalize word too
+        if (bounded_levenshtein(word, lower_pattern, MAX_DIST) <= MAX_DIST) {
+            fclose(fp);
+            return 1;
+        }
+    }
+
+    fclose(fp);
+    return 0;
 }
